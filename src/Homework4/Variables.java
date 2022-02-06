0b2cda546ca5fa480042818a688bd54e8003fe2a
adriana <andreea.condrea.98@mail.ru>
2022-02-06 16:10:37 +0200

    variables

---
+++ src/Homework4/Variables.java
@@ -0,0 +1,20 @@
+package Homework4;
+
+public class Variables {
+    public static void main(String[] args) {
+        long currentworldwpopulation = 7811080815L;
+        long carsproducedthisyear = 55235358;
+        String firstletterinalphabet = "A";
+        boolean Javaisamazing = true;
+        float pi=3.14159f;
+
+printf(firstletterinalphabet);
+
+    }
+
+    public static void printf(String firstletterinalphabet) {
+        System.out.println(firstletterinalphabet);
+    }
+
+}
+
