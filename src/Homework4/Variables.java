9296fd2aa6a1f5b695690f87ae82b8ffa349c800
adriana <andreea.condrea.98@mail.ru>
2022-02-07 15:48:51 +0200

    variables

--- src/Homework4/Variables.java
+++ src/Homework4/Variables.java
@@ -2,19 +2,16 @@ package Homework4;
 
 public class Variables {
     public static void main(String[] args) {
-        long currentworldwpopulation = 7811080815L;
-        long carsproducedthisyear = 55235358;
-        String firstletterinalphabet = "A";
-        boolean Javaisamazing = true;
-        float pi=3.14159f;
-
-printf(firstletterinalphabet);
-
+        long currentWorldPopulation = 7811080815L;
+        long carsProducedThisYear = 55235358;
+        String firstLetterInAlphabet = "A";
+        boolean javaisAmazing = true;
+        float pi = 3.14159f;
+        System.out.println("currentWorldPopulation=" + currentWorldPopulation);
+        System.out.println("carsProducedThisYear=" + carsProducedThisYear);
+        System.out.println("firstLetterInAlphabet=" + firstLetterInAlphabet);
+        System.out.println("javaisAmazing=" + javaisAmazing);
+        System.out.println("pi=" + pi);
     }
-
-    public static void printf(String firstletterinalphabet) {
-        System.out.println(firstletterinalphabet);
-    }
-
 }
 
